df0e23a50ca7bafba1ba2c27fdb80ed0abef21b2
Jose G Zuniga <joseg93151@outlook.com>
2024-03-26 18:15:54 -0600

    upload new components-helpers etc

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -2,6 +2,7 @@ import { useLocation, useNavigate } from "react-router";
 import { useForm } from "../../hooks/useForm"
 import { HeroCard } from "../components"
 import queryString from "query-string";
+import { getHeroesByName } from "../helpers";
 
 export const SearchPage = () => {
 
@@ -9,15 +10,21 @@ export const SearchPage = () => {
   const location = useLocation();
 
   const { q ='' } = queryString.parse(location.search);
+
+  const heroes = getHeroesByName(q);
+
+  const showSearch = (q.length === 0) ;
+
+  const showError = (q.length > 0) && heroes.length === 0;
   
   const { searchText, onInputChange } =useForm({
-    searchText:''
+    searchText: q
   });
 
   const onSearchSubmit = ( event ) => {
     event.preventDefault();
 
-    if( searchText.trim().length <= 1) return;
+    //if( searchText.trim().length <= 1) return;
 
     //This instruction is for use query parameter into ther URL
     navigate(`?q=${searchText}`)
@@ -56,13 +63,21 @@ export const SearchPage = () => {
       <h4>Results</h4>
       <hr/>
 
-      <div className="alert alert-primary">
-        Search a hero
-      </div>
+      {/*
+        ( q === '')
+        ? <div className="alert alert-primary"> Search a hero </div>
+        : ( heroes.length === 0 ) && <div className="alert alert-danger"> No hero with <b> { q } </b> </div>
+      }*/}
+      
+      <div className="alert alert-primary animate__animated animate__fadeIn" style={{ display: showSearch ? '' : 'none' }}> Search a hero </div>
+      
+      <div className="alert alert-danger animate__animated animate__fadeIn" style={{ display: showError ? '' : 'none' }}> No hero with <b> { q } </b> </div>
 
-      <div className="alert alert-danger">
-        No hero with <b> { q } </b>
-      </div>
+      {
+        heroes.map( hero => (
+          <HeroCard key={ hero.id } { ...hero } />
+        ))
+      }
 
      
     </div>
